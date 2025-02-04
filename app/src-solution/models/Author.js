c22582142978cc94cf36d5757648cfc1b4c494ce
benspector-mls <ben@marcylabschool.org>
2025-02-04 14:11:36 -0500

    fixing solution and starter

--- app/src-solution/models/Author.js
+++ app/src-solution/models/Author.js
@@ -3,7 +3,30 @@ import Book from "./Book.js"
 
 // build the class that would have many things
 class Author {
+  static #allAuthors = [];
+  #books = [];
 
+  constructor(name) {
+    this.id = getId();
+    this.name = name; // Array to store books
+    Author.#allAuthors.push(this);
+  };
+
+  addBook(title) {
+    this.#books.push(new Book(title, this.name));
+  };
+
+  getBooks() {
+    return [...this.#books];
+  };
+
+  static getAllAuthors() {
+    return [...Author.#allAuthors];
+  }
+
+  static findBy(id) {
+    return Author.#allAuthors.find((author) => author.id === id);
+  }
 }
 
 export default Author;
--- app/src/models/Book.js
+++ app/src/models/Book.js
@@ -2,7 +2,11 @@ import getId from "../utils/getId.js";
 
 // build the class that would belong to the things that has many things
 class Book {
-
+  constructor(title, author) {
+    this.id = getId();
+    this.title = title;
+    this.author = author;
+  }
 }
 
 export default Book;

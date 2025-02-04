ebd9cf2ffb729cee2136807ae156648bb511d796
benspector-mls <ben@marcylabschool.org>
2025-02-04 11:30:51 -0500

    adding solution and blank starter code, comments, and styling

--- app/src/models/has-many.js
+++ app/src/models/has-many.js
@@ -1,30 +1,9 @@
 import getId from "../utils/getId.js";
-import Book from "./belongs-to.js"
+import Book from "./belongs-to.js";
 
 // build the class that would have many things
-export class Author {
-  static #allAuthors = [];
-  #books = [];
+class Author {
 
-  constructor(name) {
-    this.id = getId();
-    this.name = name; // Array to store books
-    Author.#allAuthors.push(this);
-  };
-
-  addBook(title) {
-    this.#books.push(new Book(title, this.name));
-  };
-
-  getBooks() {
-    return [...this.#books];
-  };
-
-  static getAllAuthors() {
-    return [...Author.#allAuthors];
-  }
-
-  static findBy(id) {
-    return Author.#allAuthors.find((author) => author.id === id);
-  }
 }
+
+export default Author

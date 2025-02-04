2b32fd607a6994f47fde6a72b17dac034ab4abdb
benspector-mls <ben@marcylabschool.org>
2025-02-04 14:11:49 -0500

    removing book solution

--- app/src/models/Book.js
+++ app/src/models/Book.js
@@ -2,11 +2,7 @@ import getId from "../utils/getId.js";
 
 // build the class that would belong to the things that has many things
 class Book {
-  constructor(title, author) {
-    this.id = getId();
-    this.title = title;
-    this.author = author;
-  }
+
 }
 
 export default Book;
